2da262f899b7a09c429bc4af15c251c03da5ffda
mewillsh <harshsingh22082003@gmail.com>
2025-08-02 15:49:04 +0530

    hello

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyCreationData } from '../assets/assets'
 
 const Dashboard = () => {
+  const [creation,setCreations]=useState([])
+  const getDashboardData=async()=>{
+    setCreations(dummyCreationData)
+  }
+  useEffect(()=>{
+    getDashboardData()
+  },[])
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className='h-full overflow-y-scroll p-6'>
+      <div className='flex justify-start gap-4 flex-wrap'>
+        
+      </div>
     </div>
   )
 }
