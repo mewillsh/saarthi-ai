03fb2696ee0d4bba460ee2c2efe6ee544d9d36de
mewillsh <harshsingh22082003@gmail.com>
2025-08-04 13:22:40 +0530

    completed the creation of dashboard and resolved the problem of recently created markdowm

--- client/src/components/CreationItems.jsx
+++ client/src/components/CreationItems.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Markdown from 'react-markdown'
 const CreationItems = ({item}) => {
+
+  const [expanded,setExpanded]=useState(false);
+
   return (
-    <div className='p-4 max-w-5xl text-sm bg-white border border-grey-200 rounded-lg cursor-pointer'>
+    <div onClick={()=>setExpanded(!expanded)} className='p-4 max-w-5xl text-sm bg-white border border-gray-200 rounded-lg cursor-pointer'>
       <div className='flex justify-between items-center gap-4'>
         <div>
           <h2>{item.prompt}</h2>
@@ -10,6 +13,21 @@ const CreationItems = ({item}) => {
         </div>
         <button className='bg-[#EFF6FF] border border-[#BFDBFE] text-[#1E40AF] px-4 py-1 rounded-full'>{item.type}</button>
       </div>
+      {
+        expanded && (
+          <div>
+            {item.type==='image' ? (
+              <div>
+                <img src={item.content} alt="image" className='mt-3 w-full max-w-md'/>
+              </div>
+            ):(
+              <div className='prose prose-sm max-w-none'>
+                <Markdown>{item.content}</Markdown>
+                </div>
+            )}
+          </div>
+        )
+      }
     </div>
   )
 }
